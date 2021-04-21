c7828838015b75f12c38b456fbcd0d4efda5b070
Michael Doyle <hello@michaeldoyle.co>
2021-04-21 15:03:02 +0100

    Floating animation for loading state

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="absolute flex items-center justify-center w-full h-full">
-		<span class="text-2xl text-white sm:text-3xl 2xl:text-4xl font-rounded text-shadow">Loading...</span>
+	<div class="absolute z-50 flex items-center justify-center w-full h-full">
+		<span class="text-2xl text-white sm:text-3xl 2xl:text-4xl animate-float font-rounded text-shadow">Loading...</span>
 	</div>
 </template>
 
